6ec7befebb465c92202fd8eda1daba79ec18667e
Pablo Antonio Camacho Jr <bringerofawesomefood@gmail.com>
2021-11-25 13:04:55 -0700

    Updated README with instructions.

--- README.md
+++ README.md
@@ -5,3 +5,66 @@ This is code made to help with making 2D generative art for video games.
 The rendering is abstracted to any graphical library that can draw a point, line, rectangle, triangle, circle.
 
 ## Still developing. It is not ready for serious use.
+
+## How to use
+
+1. Choose a graphics library to use to handle drawing for this code set. (SDL2,raylib,etc.)
+
+2. Copy ggg2d-core folder into your project.
+
+3. Copy vanilla draw definitions header, ggg-2d-draw-defs-vanilla.h, and rename the file. 
+For best practice, include graphics library file in name. 
+Example: ggg-2d-draw-defs-raylib-3p5.h
+
+4. Use graphics library calls to define the drawing functions in the new draw defintions header.
+
+Example: ggg-2d-draw-defs-raylib-3p5.h
+
+```
+
+#include "raylib.h"
+
+#if defined(GGG2D_DRAW)
+
+
+//C definition code start
+
+void GGG2D_DrawPoint_internal(int x, int y, GGG2D_Color* color)
+{
+	//raylib draw pixel used to define GGG2D_DrawPoint_internal function
+	DrawPixel(x, y, (Color){.r=color->red,.g=color->green,.b=color->blue,.a=color->alpha});
+}
+
+...
+
+//C definition code end
+
+```
+
+5. In ggg-2d-draw-select.h, include the fully-defined draw defintions header file made in step 3 and 4.
+
+Example:
+
+```
+//define draw functions
+#define GGG2D_DRAW
+
+//add draw defintions header here.
+#include "ggg-2d-draw-defs-raylib3p5.h"
+
+```
+
+6. In the general code, include ggg-2d.h header. 
+Also include graphics library header for initialization of graphics library.
+Look at tests/animate-monster-srpite/raylib-test/animate-monster-sprite-raylib.c for an example.
+
+```
+//graphics library here
+#include "raylib.h"
+
+#include "ggg-2d.h"
+
+```
+
+7. In compilation step, add ggg-2d.c file 
+Look at tests/animate-monster-sprite/raylib-test/Makefile for an example.
--- c-code/ggg2d-core/ggg-2d-draw-defs-vanilla.h
+++ c-code/ggg2d-core/ggg-2d-draw-defs-vanilla.h
@@ -1,14 +1,60 @@
 
+#include "ggg-2d-defs.h"
+#include "raylib.h"
+
+
 #ifndef GGG2D_DRAW_DEFS_H
 #define GGG2D_DRAW_DEFS_H
 
-#include "ggg-2d-defs.h"
+//C declaration code
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-//C code start
+//function to draw a point with color
+void GGG2D_DrawPoint_internal(int x, int y, GGG2D_Color* color);
+
+//function to draw a point with color
+void GGG2D_DrawPointP_internal(GGG2D_Point p1, GGG2D_Color* color);
+
+//function to draw line with thickness and color
+void GGG2D_DrawLine_internal(int x1, int y1, int x2, int y2, int thickness, GGG2D_Color* color);
+
+//function to draw line with thickness and color
+void GGG2D_DrawLineP_internal(GGG2D_Point p1, GGG2D_Point p2, int thickness, GGG2D_Color* color);
+
+//function to draw rectangled, filled
+void GGG2D_DrawRectangle_internal(GGG2D_Rectangle* rect,GGG2D_Color* color);
+
+//function to draw rectangle outline with color
+void GGG2D_DrawRectangleOutline_internal(GGG2D_Rectangle* rect,GGG2D_Color* color);
+
+//function to draw triangle, filled
+void GGG2D_DrawTriangle_internal(GGG2D_Triangle* triangle,GGG2D_Color* color);
+
+//function to draw triangle outline with color
+void GGG2D_DrawTriangleOutline_internal(GGG2D_Triangle* triangle,GGG2D_Color* color);
+
+//function to draw circle, filled
+void GGG2D_DrawCircle_internal(GGG2D_Circle* circle, GGG2D_Color* color);
+
+//function to draw circle outline with color
+void GGG2D_DrawCircleOutline_internal(GGG2D_Circle* circle, GGG2D_Color* color);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+//C declaration code end
+
+#if defined(GGG2D_DRAW)
+
+
+//C definition code start
+
 
 //function to draw a point with color
 void GGG2D_DrawPoint_internal(int x, int y, GGG2D_Color* color)
@@ -25,7 +71,7 @@ void GGG2D_DrawPointP_internal(GGG2D_Point p1, GGG2D_Color* color)
 //function to draw line with thickness and color
 void GGG2D_DrawLine_internal(int x1, int y1, int x2, int y2, int thickness, GGG2D_Color* color)
 {
-	
+
 }
 
 //function to draw line with thickness and color
@@ -72,8 +118,6 @@ void GGG2D_DrawCircleOutline_internal(GGG2D_Circle* circle, GGG2D_Color* color)
 
 //end C code
 
-#ifdef __cplusplus
-}
-#endif
+
 
 #endif
